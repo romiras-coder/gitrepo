ad15fc37c64ddf28630bc1c88908e21b8d79282c
romiras-coder <rom_iras@mail.ru>
2019-04-19 22:10:10 +0300

    прошел урок 2

---
+++ Lesson-2.py
@@ -0,0 +1,52 @@
+#
+#
+# print("Hello" + " " + "World")
+#
+# print("Hello" " "  "world")
+#
+# print("Hi!" * 3)
+#
+# st = "Python самый знатный, незаурядный и сильный язык программирования"
+#
+# slice(start, stop, step)
+
+
+# # name = "Alex"
+# # soname = "Moskotlinov"
+#
+# # print("Приветствуем, {1} {0} на нашем сайте".format(soname, name))
+#
+# # print(f"Приветствуем, {name} {soname} на нашем сайте")
+#
+# print(f"{100*5}")
+#
+
+# m = [1, 2, 3, 4, 5, 7.45, "sada", False]
+# b = [1, 2, 3, [11, 22, 33], 4, 5]
+# m[2] = "New"
+#
+# m[0:3] = [3, 6, 9]
+#
+# m[:0] = ["True"]
+#
+# m.append(200)
+#
+# print(b[3] [0])
+#
+# print(m)
+
+
+russian_fr = ["свекла", "морковь", "капуста"]
+
+# i = 0
+#
+# while len(russian_fr) > i:
+#     print("фрукты России - ", russian_fr[i])
+#     i += 1
+# print()
+
+for f in russian_fr:
+    print("фрукты России - ", f)
+
+for el in "Привет":
+    print("Буква - ", el)
